69a9bd3797106975aa8f5dfc5fcdaa7343e7813e
Your Name <you@example.com>
2022-04-26 21:24:45 +0700

    KTLT

---
+++ Tuần 5/Bài 1/Bài 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_1
+{
+    internal class Program
+    {
+        static int n, k;
+        static int[] ChinhHopLap;
+        static void Print()
+        {
+            for (int j = 1; j <= k; j++)
+                Console.Write(ChinhHopLap[j] + " ");
+            Console.WriteLine();
+        }
+        static void Try(int i)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                ChinhHopLap[i] = j;
+                if (i == k)
+                    Print();
+                else
+                    Try(i + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nhap n, k: ");
+            string[] token = Console.ReadLine().Split();
+            n = int.Parse(token[0]);
+            k = int.Parse(token[1]);
+            ChinhHopLap = new int[n + 1];
+            Console.WriteLine(Math.Pow(n, k));
+            Try(1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tuần 5/Bài 4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_4
+{
+    internal class Program
+    {
+        static int n;
+        static int[] Solution;
+        static int Count = 0;
+        static bool Check(int x, int y)
+        {
+            for (int i = 1; i < x; i++)
+                if (Solution[i] == y || Math.Abs(i - x) == Math.Abs(Solution[i] - y))
+                    return false;
+            return true;
+        }
+        static void Print(int n)
+        {
+            Console.Write($"Solution {Count}: ");
+            for (int i = 1; i <= n; i++)
+                Console.Write(Solution[i] + " ");
+            Console.WriteLine();
+        }
+        static void Try(int i, int n)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (Check(i, j))
+                {
+                    Solution[i] = j;
+                    if (i == n)
+                    {
+                        Count += 1;
+                        Print(n);
+                    }
+                    Try(i + 1, n);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            n = 8;
+            Solution = new int[n + 1];
+            Try(1, n);
+            Console.ReadKey();
+        }
+    }
+}
